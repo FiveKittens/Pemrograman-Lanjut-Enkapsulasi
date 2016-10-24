d74c29e7d593482cf9cb175088a00c94dafdf0d8
FiveKittens <udz.elegant@gmail.com>
2016-10-24 08:43:38 +0700

    Pemrograman Lanjut
    Enkappsulasi

---
+++ Mahasiswa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Mahasiswa
+    {
+        private string nim;
+        private string nama;
+        private double nilai;
+
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public double Nilai { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char jwb = 'y';
+            while(jwb == 'y') { 
+            Mahasiswa mhs = new Mahasiswa();
+            Console.Write("Nama\t: ");
+            mhs.Nama = Console.ReadLine();
+            Console.Write("NIM\t: ");
+            mhs.Nim = Console.ReadLine();
+            Console.Write("Nilai\t: ");
+            mhs.Nilai = Int16.Parse(Console.ReadLine());
+                Console.ReadLine();
+            if(mhs.Nilai > 75)
+            {
+                Console.WriteLine("LULUS");
+            }
+            else
+            {
+                Console.WriteLine("TIDAK LULUS");
+            }
+                Console.ReadLine();
+                Console.Write("Apakah anda ingin mengulangi? (y/n) ");
+                jwb = char.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
